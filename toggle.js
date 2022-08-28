1f4a7aadfe90dca0d3413a3973cf9d7f09ef2143
ChunYi <jokersaysjoke@gmail.com>
2022-08-29 01:20:43 +0800

    iv

--- toggle.js
+++ toggle.js
@@ -1,11 +1,13 @@
 const button = document.getElementById("toggle");
 const body = document.querySelector("body");
-const h1 = document.querySelector("h1");
 
 
 
 button.onclick=function(){
     button.classList.toggle('light');
     body.classList.toggle('light');
-    h1.classList.toggle('light');
 }
+
+mediumZoom('.zoom',{
+    margin: 50
+})
